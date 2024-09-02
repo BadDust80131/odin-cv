6c32d26f309911811fa9be577b9f35f67132cd24
BadDust80131 <45376544+BadDust80131@users.noreply.github.com>
2024-09-01 21:03:12 -0700

    basic ui coming together

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import "./App.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
 
-function App() {}
+function App() {
+  return (
+    <Accordion type="single" defaultValue="item-1" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>General Information</AccordionTrigger>
+        <AccordionContent>
+          <Input />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
 
 export default App;
